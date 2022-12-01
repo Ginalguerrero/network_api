49f3cc35ee638c7b3ba69c12728f8f18fe839c39
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-01 12:15:34 -0800

    start the application on server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const db = require("./config/conn");
+const routes = require("./routes");
+
+const PORT = 3001;
+const app = express();
+const {urlencoded, json} = express;
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(routes);
+
+db.once("open", () =>
+  app.listen(PORT, () => console.log(`Running at port: ${PORT}!`))
+);
