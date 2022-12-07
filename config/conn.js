180d32d5f522ba6d551c093188d5d8eea036c8db
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-07 09:59:13 -0800

    added express and sequel

--- config/conn.js
+++ config/conn.js
@@ -1,12 +1,10 @@
+const { connect, connection } = require("mongoose");
 
-const { connect, connection } = require('mongoose')
+const connectionString = "mongodb://localhost:27017/networkDB";
 
-const connectionString = 'mongodb://localhost:27017/networkDB';
-
-console.log(connectionString)
 connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 module.exports = connection;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,17 @@
-const router = require("express").Router()
-const userController = require("../controllers/user.controllers")
+const router = require("express").Router();
+const userController = require("../controllers/user.controllers");
 
-router.route('/').get(userController.getUsers).post(userController.createUser);
+router.route("/").get(userController.getUsers).post(userController.createUser);
 
-router.route('/:userId').get(userController.getUser).delete(userController.deleteUser).put(userController.updateUser);
+router
+  .route("/:userId")
+  .get(userController.getUser)
+  .delete(userController.deleteUser)
+  .put(userController.updateUser);
 
-router.route('/:userId/friends/:friendId').post(userController.addFriend).delete(userController.removeFriend);
+router
+  .route("/:userId/friends/:friendId")
+  .post(userController.addFriend)
+  .delete(userController.removeFriend);
 
-module.exports = router;
+module.exports = { router };
